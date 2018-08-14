72687c85a75e536b6f99ec05b3dc02f5f59e2bee
Caiyishuai <39987654+Caiyishuai@users.noreply.github.com>
2018-08-14 21:23:11 +0800

    Add files via upload

---
+++ 定时器/Timer/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace MyTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxSecond2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSecond1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxColon2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMinute2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMinute1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxColon1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxHour2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxHour1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSecond2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSecond1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColon2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMinute2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMinute1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHour2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHour1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonStart.ForeColor = System.Drawing.Color.Black;
+            this.buttonStart.Location = new System.Drawing.Point(12, 75);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(58, 30);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "开始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Enabled = false;
+            this.buttonPause.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonPause.ForeColor = System.Drawing.Color.Black;
+            this.buttonPause.Location = new System.Drawing.Point(83, 75);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(58, 30);
+            this.buttonPause.TabIndex = 6;
+            this.buttonPause.Text = "暂停";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonStop.ForeColor = System.Drawing.Color.Black;
+            this.buttonStop.Location = new System.Drawing.Point(154, 75);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(58, 30);
+            this.buttonStop.TabIndex = 7;
+            this.buttonStop.Text = "停止";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBoxSecond2
+            // 
+            this.pictureBoxSecond2.Location = new System.Drawing.Point(183, 10);
+            this.pictureBoxSecond2.Name = "pictureBoxSecond2";
+            this.pictureBoxSecond2.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxSecond2.TabIndex = 23;
+            this.pictureBoxSecond2.TabStop = false;
+            // 
+            // pictureBoxSecond1
+            // 
+            this.pictureBoxSecond1.Location = new System.Drawing.Point(158, 10);
+            this.pictureBoxSecond1.Name = "pictureBoxSecond1";
+            this.pictureBoxSecond1.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxSecond1.TabIndex = 21;
+            this.pictureBoxSecond1.TabStop = false;
+            // 
+            // pictureBoxColon2
+            // 
+            this.pictureBoxColon2.Location = new System.Drawing.Point(135, 10);
+            this.pictureBoxColon2.Name = "pictureBoxColon2";
+            this.pictureBoxColon2.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxColon2.TabIndex = 22;
+            this.pictureBoxColon2.TabStop = false;
+            // 
+            // pictureBoxMinute2
+            // 
+            this.pictureBoxMinute2.Location = new System.Drawing.Point(111, 10);
+            this.pictureBoxMinute2.Name = "pictureBoxMinute2";
+            this.pictureBoxMinute2.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxMinute2.TabIndex = 20;
+            this.pictureBoxMinute2.TabStop = false;
+            // 
+            // pictureBoxMinute1
+            // 
+            this.pictureBoxMinute1.Location = new System.Drawing.Point(86, 10);
+            this.pictureBoxMinute1.Name = "pictureBoxMinute1";
+            this.pictureBoxMinute1.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxMinute1.TabIndex = 19;
+            this.pictureBoxMinute1.TabStop = false;
+            // 
+            // pictureBoxColon1
+            // 
+            this.pictureBoxColon1.Location = new System.Drawing.Point(61, 10);
+            this.pictureBoxColon1.Name = "pictureBoxColon1";
+            this.pictureBoxColon1.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxColon1.TabIndex = 18;
+            this.pictureBoxColon1.TabStop = false;
+            // 
+            // pictureBoxHour2
+            // 
+            this.pictureBoxHour2.Location = new System.Drawing.Point(36, 10);
+            this.pictureBoxHour2.Name = "pictureBoxHour2";
+            this.pictureBoxHour2.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxHour2.TabIndex = 17;
+            this.pictureBoxHour2.TabStop = false;
+            // 
+            // pictureBoxHour1
+            // 
+            this.pictureBoxHour1.Location = new System.Drawing.Point(11, 10);
+            this.pictureBoxHour1.Name = "pictureBoxHour1";
+            this.pictureBoxHour1.Size = new System.Drawing.Size(28, 47);
+            this.pictureBoxHour1.TabIndex = 16;
+            this.pictureBoxHour1.TabStop = false;
+            this.pictureBoxHour1.Click += new System.EventHandler(this.pictureBoxHour1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.CancelButton = this.buttonStop;
+            this.ClientSize = new System.Drawing.Size(226, 119);
+            this.Controls.Add(this.pictureBoxSecond2);
+            this.Controls.Add(this.pictureBoxSecond1);
+            this.Controls.Add(this.pictureBoxColon2);
+            this.Controls.Add(this.pictureBoxMinute2);
+            this.Controls.Add(this.pictureBoxMinute1);
+            this.Controls.Add(this.pictureBoxColon1);
+            this.Controls.Add(this.pictureBoxHour2);
+            this.Controls.Add(this.pictureBoxHour1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "计时器";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSecond2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSecond1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColon2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMinute2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMinute1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHour2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHour1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBoxSecond2;
+        private System.Windows.Forms.PictureBox pictureBoxSecond1;
+        private System.Windows.Forms.PictureBox pictureBoxColon2;
+        private System.Windows.Forms.PictureBox pictureBoxMinute2;
+        private System.Windows.Forms.PictureBox pictureBoxMinute1;
+        private System.Windows.Forms.PictureBox pictureBoxColon1;
+        private System.Windows.Forms.PictureBox pictureBoxHour2;
+        private System.Windows.Forms.PictureBox pictureBoxHour1;
+    }
+}
+
---
+++ 定时器/Timer/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//添加以下命名空间
+using System.IO;
+using System.Reflection; 
+using System.Resources;
+
+namespace MyTimer
+{
+    public partial class Form1 : Form
+    {
+        //类字段变量声明
+
+        //类字段：当前的时间计数值(以秒为单位)
+        private int _nowSecond = 0;
+        //类字段：冒号控制标志
+        private Boolean _colonFlag = true;
+
+        //数码管位图数组
+        private Bitmap[] _bmpShow = new Bitmap[13];
+
+        //自定义方法：由显示字符获取对应的数码管位图
+        private Image GetResourceImage(string displayStr)
+        {
+            if (displayStr == "0")
+                return _bmpShow[0];
+            else if (displayStr == "1")
+                return _bmpShow[1];
+            else if (displayStr == "2")
+                return _bmpShow[2];
+            else if (displayStr == "3")
+                return _bmpShow[3];
+            else if (displayStr == "4")
+                return _bmpShow[4];
+            else if (displayStr == "5")
+                return _bmpShow[5];
+            else if (displayStr == "6")
+                return _bmpShow[6];
+            else if (displayStr == "7")
+                return _bmpShow[7];
+            else if (displayStr == "8")
+                return _bmpShow[8];
+            else if (displayStr == "9")
+                return _bmpShow[9];
+            else if (displayStr == ".")
+                return _bmpShow[10];
+            else if (displayStr == ":")
+                return _bmpShow[11];
+            else
+                return _bmpShow[12];
+        }
+
+        //构造函数
+        public Form1()
+        {
+            InitializeComponent();
+            //初始化时间计数值(秒)
+            _nowSecond = 0;
+            //装载数码管位图文件(注意：要保证Debug目录下有images子目录及bmp文件，否则会报错)
+            _bmpShow[0] = new Bitmap("images/0.bmp");
+            _bmpShow[1] = new Bitmap("images/1.bmp");
+            _bmpShow[2] = new Bitmap("images/2.bmp");
+            _bmpShow[3] = new Bitmap("images/3.bmp");
+            _bmpShow[4] = new Bitmap("images/4.bmp");
+            _bmpShow[5] = new Bitmap("images/5.bmp");
+            _bmpShow[6] = new Bitmap("images/6.bmp");
+            _bmpShow[7] = new Bitmap("images/7.bmp");
+            _bmpShow[8] = new Bitmap("images/8.bmp");
+            _bmpShow[9] = new Bitmap("images/9.bmp");            
+            _bmpShow[10] = new Bitmap("images/dot1.bmp");
+            _bmpShow[11] = new Bitmap("images/dot2.bmp");
+            _bmpShow[12] = new Bitmap("images/blank.bmp");
+        }
+
+        //窗口的Load事件响应方法
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化所有ImageBox控件的初始位图
+            pictureBoxHour1.Image = GetResourceImage("0");
+            pictureBoxHour2.Image = GetResourceImage("0");
+            pictureBoxMinute1.Image = GetResourceImage("0");
+            pictureBoxMinute2.Image = GetResourceImage("0");
+            pictureBoxSecond1.Image = GetResourceImage("0");
+            pictureBoxSecond2.Image = GetResourceImage("0");
+            pictureBoxColon1.Image = GetResourceImage(":");
+            pictureBoxColon2.Image = GetResourceImage(":");
+        }     
+
+        //Timer1定时器的Tick事件响应方法
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //时间计数值(秒)加1
+            _nowSecond++;
+            //计算当前的小时、分、秒
+            int hour = _nowSecond / 3600;
+            int minute = (_nowSecond % 3600) / 60;
+            int second = (_nowSecond % 3600) % 60;
+            //显示当前的秒
+            this.pictureBoxSecond1.Image = GetResourceImage((second / 10).ToString());
+            this.pictureBoxSecond2.Image = GetResourceImage((second % 10).ToString());
+            //显示当前的分
+            this.pictureBoxMinute1.Image = GetResourceImage((minute / 10).ToString());
+            this.pictureBoxMinute2.Image = GetResourceImage((minute % 10).ToString());
+            //显示当前的小时
+            this.pictureBoxHour1.Image = GetResourceImage((hour / 10).ToString());
+            this.pictureBoxHour2.Image = GetResourceImage((hour % 10).ToString());
+        }
+
+        //Timer2定时器的Tick事件响应方法
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //切换冒号显示控制标志
+            _colonFlag = !_colonFlag;
+
+            //判断冒号控制标志是否为true
+            if (_colonFlag == true)
+            {
+                pictureBoxColon1.Image = GetResourceImage(" ");
+                pictureBoxColon2.Image = GetResourceImage(" ");
+            }
+            else
+            {
+                pictureBoxColon1.Image = GetResourceImage(":");
+                pictureBoxColon2.Image = GetResourceImage(":");
+            }
+        }
+
+        //按钮"开始"的Click事件响应方法
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            //启用定时器
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            //启用或禁用有关按钮
+            buttonStart.Enabled = false;
+            buttonPause.Enabled = true;
+            buttonStop.Enabled = true;
+            //冒号控制标志设置为true
+            _colonFlag = true;
+            //隐藏冒号
+            pictureBoxColon1.Image = GetResourceImage(" ");
+            pictureBoxColon2.Image = GetResourceImage(" ");
+        }
+
+        //按钮"暂停"的Click事件响应方法
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            //禁用定时器
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            //启用或禁用有关按钮
+            buttonStart.Enabled = true;
+            buttonPause.Enabled = false;
+            buttonStop.Enabled = false;
+            //冒号控制标志设置为true
+            _colonFlag = true;
+            //显示冒号
+            pictureBoxColon1.Image = GetResourceImage(":");
+            pictureBoxColon2.Image = GetResourceImage(":");
+        }
+
+        //按钮"停止"的Click事件响应方法
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            //禁用定时器
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            //启用或禁用有关按钮
+            buttonStart.Enabled = true;
+            buttonPause.Enabled = false;
+            buttonStop.Enabled = false;
+            //冒号控制标志设置为true
+            _colonFlag = true;
+            //显示冒号
+            pictureBoxColon1.Image = GetResourceImage(":");
+            pictureBoxColon2.Image = GetResourceImage(":");
+
+            //清空时间计数值(秒)
+            _nowSecond = 0;
+        }
+
+        //窗口的Move(窗口移动)事件响应方法
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            //停靠屏幕左右边框
+            int screenRight = Screen.PrimaryScreen.Bounds.Right;
+            int formRight = this.Left + this.Size.Width;
+            if (Math.Abs(screenRight - formRight) <= 100)
+                this.Left = screenRight - this.Size.Width;
+            if (Math.Abs(this.Left) <= 100)
+                this.Left = 0;
+
+            //停靠屏幕上下边框
+            int screenBottom = Screen.PrimaryScreen.Bounds.Bottom;
+            int formBottom = this.Top + this.Size.Height;
+            if (Math.Abs(screenBottom - formBottom) <= 60)
+                this.Top = screenBottom - this.Size.Height;
+            if (Math.Abs(this.Top) <= 100)
+                this.Top = 0;
+        }
+
+        private void pictureBoxHour1_Click(object sender, EventArgs e)
+        {
+
+        }           
+    }
+}
